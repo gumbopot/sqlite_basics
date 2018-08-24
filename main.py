bc2803c9c2ce16cc92803f6f032a539e44d77fa3
gumbopot <hyatt1ea@gmail.com>
2018-08-24 11:43:10 -0400

    Added dynamic insert and select examples

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-import sqlite3
+import sqlite3, datetime, time, random
 
 # Create connection
 conn = sqlite3.connect('tutorial.db')
@@ -21,7 +21,43 @@ def data_entry():
 	c.close()
 	conn.close()
 
-def data_entry():
+def dynamic_data_entry():
 	unix = time.time()
-	data = str(datetime.datetime.fromtimestamp(unix).strftime('%Y-%m-%d %H:%M:%S'))
+	date = str(datetime.datetime.fromtimestamp(unix).strftime('%Y-%m-%d %H:%M:%S'))
+	keyword = 'python'
+	value = random.randrange(0,10)
+	c.execute("INSERT INTO stuffToPlot (unix, datestamp, keyword, value) VALUES (?, ?, ?, ?)",
+	(unix, date, keyword, value))
+	conn.commit()
+	# don't need to open/close the connection everytime you commit
 
+
+def read_from_db():
+	# Select statement finds the data according to the query
+	# you must then do something with the data
+	#c.execute('SELECT * FROM stuffToPlot')
+	c.execute("SELECT * FROM stuffToPlot WHERE unix > 1535121381")
+
+	# data = c.fetchone()	# fetch single row
+	# data = c.fetchall()
+
+	# print(data)
+
+	# Iterating then printing makes it much more readable
+	for row in c.fetchall():
+		print(row)
+
+
+# create_table()
+# data_entry()
+
+# for i in range(10):
+# 	dynamic_data_entry()
+# 	time.sleep(1)
+
+
+read_from_db()
+
+
+c.close()
+conn.close()
